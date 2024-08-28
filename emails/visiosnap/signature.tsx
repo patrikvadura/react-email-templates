1c498d95c5495e562ab75c63b2b8f0c01286810d
Patrik Vaďura <patrikvadura@icloud.com>
2024-08-28 16:31:47 +0200

    signature - visiosnap

--- emails/visiosnap/signature.tsx
+++ emails/visiosnap/signature.tsx
@@ -10,13 +10,19 @@ import {
   Section,
   Text,
   Tailwind,
+  Button,
+  Row,
+  Column,
 } from '@react-email/components'
 import * as React from 'react'
-import { Fragment } from 'react'
 
 const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : ''
+const logoImage = `https://profile-next-core.s3.eu-north-1.amazonaws.com/identity/logo_default.png`
+const userImage = `${baseUrl}/static/visiosnap/email_photo/email-photo_jakub_light.png`
+const userName = 'Jakub Galia'
+const infoEmail = 'jakub@visiosnap.cz'
 
-export const VercelInviteUserEmail = () => {
+export const VisioSnapSignature = () => {
   return (
     <Html>
       <Tailwind
@@ -28,8 +34,8 @@ export const VercelInviteUserEmail = () => {
             extend: {
               colors: {
                 brand: {
-                  primary: '#29349A',
-                  secondary: '#05E988',
+                  primary: '#121FC9',
+                  secondary: '#CBFE01',
                   accent: '#FCA4ED',
                 },
               },
@@ -38,60 +44,66 @@ export const VercelInviteUserEmail = () => {
         }}
       >
         <Head />
-        <Fragment>
-          <Body className="my-auto mx-auto w-full">
-            <Container className="my-auto mx-auto font-sans px-4 sm:px-2 w-full">
-              <Container className="border-[2px] border-solid border-[#F0F1F8] rounded-xl my-[20px] sm:my-[20px] mx-auto px-[20px] sm:px-[40px] py-[40px] max-w-[465px]">
-                <Heading className="text-black text-[20px] font-semibold text-center p-0 mb-[30px] mx-0">
-                  Dobrý den,
-                </Heading>
+        <Body className="bg-[#F0F1F8] w-full">
+          <Container className="bg-[#F0F1F8] font-sans px-[10px] w-full sm:w-[600px]">
+            <Container className="bg-white rounded-xl my-[20px] sm:my-[40px] mx-auto px-[20px] sm:px-[40px] py-[40px] w-full sm:w-[600px]">
+              <Heading className="text-black text-[16px] font-semibold text-left p-0 mb-[30px] mx-0">
+                Dobrý den,
+              </Heading>
 
-                <Text className="text-center text-black text-[14px] leading-[20px] m-0 p-0">
-                  Lorem ipsum is placeholder text commonly used in the graphic, print, and
-                  publishing industries for previewing layouts and visual mockups.
-                </Text>
+              <Text className="text-left text-black text-[14px] leading-[20px] m-0 p-0">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce aliquam vestibulum
+                ipsum. Duis viverra diam non justo. Nulla accumsan, elit sit amet varius semper,
+                nulla mauris mollis quam, tempor suscipit diam nulla vel leo.
+              </Text>
 
-                <Hr className="border border-solid border-[#F0F1F8] my-[26px] mx-0 w-full" />
+              <Hr className="border border-solid border-[#eaeaea] mt-[26px] mb-[16px] mx-0 w-full" />
 
-                <Section className="flex flex-row items-center justify-center text-center w-full m-0 p-0">
-                  <Text className="text-center text-[#666666] text-[12px] leading-[20px]">
-                    <Link href="https://visiosnap.cz" className="text-[#666666]">
-                      visiosnap.cz
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="https://visiosnap.cz/help" className="text-[#666666]">
-                      Nápověda
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="https://visiosnap.cz/affiliate" className="text-[#666666]">
-                      Affiliate
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="https://visiosnap.cz/terms" className="text-[#666666]">
-                      Podmínky
-                    </Link>
-                  </Text>
-                </Section>
+              <Section className="flex justify-start items-center">
+                <Row>
+                  <Column align="left">
+                    <Img className="rounded-full pr-2 md:pr-0" src={userImage} height="80" />
+                  </Column>
 
-                <Text className="text-center text-[#666666] text-[12px] leading-[20px] m-0 p-0">
-                  Powered by
-                  <Link href="https://visiosnap.cz">
-                    <Img
-                      src={`${baseUrl}/static/visiosnap/logo_monochrom.png`}
-                      width="75"
-                      height="17"
-                      alt="VisioSnap"
-                      className="inline pl-1 translate-y-0.5 opacity-60"
-                    />
-                  </Link>
-                </Text>
-              </Container>
+                  <Column className="pl-3">
+                    <Text className="text-brand-primary text-[13px] leading-[16px] mt-3 mb-0 sm:mb-1">
+                      <span className="font-[600]">{userName}</span>
+                    </Text>
+
+                    <Text className="text-[#666666] text-[13px] leading-[17px] mt-0 mb-0">
+                      <Link href={`mailto: ${infoEmail};`} className="text-[#666666]">
+                        {infoEmail}
+                      </Link>{' '}
+                      |{' '}
+                      <Link href="https://visiosnap.cz" className="text-[#666666]">
+                        visiosnap.cz
+                      </Link>
+                    </Text>
+
+                    <Text className="text-left text-[#666666] text-[12px] leading-[16px] mt-3">
+                      <Link href="https://visiosnap.cz" className="text-[#666666]">
+                        Powered by
+                      </Link>{' '}
+                      |{' '}
+                      <Link href="https://visiosnap.cz">
+                        <Img
+                          src={`${baseUrl}/static/visiosnap/logo_monochrom.png`}
+                          width="75"
+                          height="17"
+                          alt="VisioSnap"
+                          className="inline pl-1 translate-y-1 opacity-60"
+                        />
+                      </Link>
+                    </Text>
+                  </Column>
+                </Row>
+              </Section>
             </Container>
-          </Body>
-        </Fragment>
+          </Container>
+        </Body>
       </Tailwind>
     </Html>
   )
 }
 
-export default VercelInviteUserEmail
+export default VisioSnapSignature
